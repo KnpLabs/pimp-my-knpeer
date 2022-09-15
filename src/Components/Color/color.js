28d9e984710d4b4dffea92fca04a1fc107f614b4
Manaaia <manon23.villar@hotmail.fr>
2022-09-15 17:58:37 +0200

    wip: navigation

--- src/Components/Color/color.js
+++ src/Components/Color/color.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './color.css'
 
-const Color = ({setColor, colors = []}) => {
+const Color = ({setColor, colors = [], selectedColor = null}) => {
     return (
         <div className="colors">
-            {colors.map(color => 
-                <div key={color} className={ `color ${color}`} onClick={() => setColor(color) }></div>
+            {colors.map(color =>
+                <div
+                    key={color}
+                    className={ `color ${color} ${selectedColor === color ? 'selected' : ''}`}
+                    onClick={() => setColor(color) }
+                >
+                </div>
             )}
         </div>
     )
--- src/Components/Trait/trait.js
+++ src/Components/Trait/trait.js
@@ -4,15 +4,21 @@ import Color from '../Color/color'
 import { useAvatarContext } from '../Context/avatarContext'
 
 const toggleTrait = traitName => {
+    document.querySelectorAll(`.traits:not([data-is=${traitName}]) .selection`).forEach(
+        selection => {selection.classList.add('hidden')}
+    )
+    document.querySelectorAll(`.traits:not([data-is=${traitName}]) .chevron`).forEach(
+        chevron => {chevron.setAttribute('src', '/Images/Assets/chevron-down.svg')}
+    )
+
     document.querySelector(`[data-is=${traitName}] .selection`).classList.toggle('hidden')
     const chevronSrc = document.querySelector(`[data-is=${traitName}] .chevron`).src
     document.querySelector(`[data-is=${traitName}] .chevron`).src = chevronSrc.includes('down')
         ? '/Images/Assets/chevron-up.svg'
         : '/Images/Assets/chevron-down.svg'
-
 }
 
-const Trait = ({name, setColor, setTrait, selectedTrait = null, traits = [], colors = []}) => {
+const Trait = ({name, setColor, setTrait, selectedColor = null, selectedTrait = null, traits = [], colors = []}) => {
     const avatar = useAvatarContext()
 
     return (
@@ -23,12 +29,12 @@ const Trait = ({name, setColor, setTrait, selectedTrait = null, traits = [], col
             </div>
 
             <div className="selection hidden">
-                {colors.length > 0 && <Color colors={colors} setColor={setColor} />}
+                {colors.length > 0 && <Color colors={colors} setColor={setColor} selectedColor={selectedColor} />}
                 {traits.map(Trait =>
                     <button
-                        className="trait"
+                        className={`trait ${selectedTrait === Trait.render.name ? 'selected' : ''}`}
                         key={'button' + Trait.render.name}
-                        onClick={() => setTrait(Trait.render.name) }
+                        onClick={() => setTrait(selectedTrait === Trait.render.name ? null : Trait.render.name)}
                     >
                         <Trait />
 
