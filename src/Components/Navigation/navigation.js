2d9b896a584c5ce4719a20fa11d1097b02da975e
Manaaia <manon23.villar@hotmail.fr>
2022-09-15 18:11:34 +0200

    wip

--- src/Components/Navigation/navigation.js
+++ src/Components/Navigation/navigation.js
@@ -5,6 +5,7 @@ const Navigation = () => {
     return (
         <div className='navigation'>
             <span><img className="previous" src="/Images/Assets/left-arrow.svg"/></span>
+
             <span><img className="next" src="/Images/Assets/right-arrow.svg"/></span>
         </div>
     )
